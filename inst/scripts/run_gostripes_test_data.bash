a74174931775a940281fc06589149cba0617f7f0
jtourig <27238615+jtourig@users.noreply.github.com>
2021-01-05 16:03:00 -0500

    testing index option command

--- inst/scripts/run_gostripes_test_data.bash
+++ inst/scripts/run_gostripes_test_data.bash
@@ -10,6 +10,7 @@ samples='/opt/conda/envs/gostripes/lib/R/library/gostripes/extdata/gostripes_exa
 yeast_assembly='/opt/conda/envs/gostripes/lib/R/library/gostripes/extdata/Saccharomyces_cerevisiae.R64-1-1.dna_sm.toplevel.fa'
 yeast_annotation='/opt/conda/envs/gostripes/lib/R/library/gostripes/extdata/Saccharomyces_cerevisiae.R64-1-1.99.gtf'
 yeast_rRNA='/opt/conda/envs/gostripes/lib/R/library/gostripes/extdata/Sc_rRNA.fasta'
+yeast_index='./Sc-R64-STAR-index/'
 num_procs=4
 output_dir='./gostripes-example-output/'
 
@@ -18,9 +19,14 @@ output_dir='./gostripes-example-output/'
 export PATH=$HOME/gostripes/inst/scripts/:$PATH
 
 ### CMD ###
+# run_gostripes.R \
+# 	--sample-sheet "$samples" --cpus "$num_procs" \
+# 	--assembly "$yeast_assembly" --annotation "$yeast_annotation" \
+# 	--rRNA "$yeast_rRNA" --output-dir "$output_dir"
+
 run_gostripes.R \
-	 --sample-sheet "$samples" --cpus "$num_procs" \
-	--assembly "$yeast_assembly" --annotation "$yeast_annotation" \
+	--sample-sheet "$samples" --cpus "$num_procs" \
+	--STAR-index "$yeast_index" \
 	--rRNA "$yeast_rRNA" --output-dir "$output_dir"
 
 exit
